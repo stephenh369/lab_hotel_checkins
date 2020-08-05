b10a2549e2f3d41162473e4c6694a3f4009c376b
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-08-05 11:40:14 +0100

    server.js completed

--- server/server.js
+++ server/server.js
@@ -7,3 +7,16 @@ const createRouter = require('./helpers/create_router');
 
 app.use(parser.json());
 app.use(cors());
+
+MongoClient.connect('mongodb://localhost:27017')
+.then(client => {
+  const db = client.db('hotel');
+  const bookingsCollection = db.collection('bookings');
+  const bookingsRouter = createRouter(bookingsCollection);
+  app.use('/api/bookings', bookingsRouter);
+})
+.catch(console.error)
+
+app.listen(3000, function () {
+  console.log(`Listening on port ${ this.address().port }`);
+});
